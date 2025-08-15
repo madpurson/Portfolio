7943bba79a4fac1ff97a05285213281278161404
Miki <110220534+madpurson@users.noreply.github.com>
2025-08-15 23:57:45 +0800

    added a navbar template from tailwind

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { NavbarDark } from './component/navbar'
 
 function App() {
 
   return (
     <>
+      <NavbarDark></NavbarDark>
       <div>EMPTY</div>
     </>
   )

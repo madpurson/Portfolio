3bb19a3d7100f0f57de398a220a5ff7420271002
Miki <110220534+madpurson@users.noreply.github.com>
2025-09-29 23:14:40 +0800

    Create AboutPageBody.jsx

---
+++ portfolio/src/components/AboutPageBody.jsx
@@ -0,0 +1,14 @@
+export function AboutPageBody() {
+    return (
+        <>
+            <h1>About.</h1>
+            <div>
+                <p>
+                    I’m a software developer with a focus on backend systems, 
+                    full-stack web development, and building real-world solutions. 
+                    I enjoy working on meaningful projects that combine code, creativity, and impact.
+                </p>
+            </div>
+        </>
+    )
+}

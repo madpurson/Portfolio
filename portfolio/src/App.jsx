7df64571e5cb888c48ae2855fbd8617490f16a3f
Miki <110220534+madpurson@users.noreply.github.com>
2025-09-29 23:14:37 +0800

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import { NavPannel } from './components/NavPannel'
-import { LandingPageBody } from './components/LandingPageBody'
+import { LandingPageBody } from './components/LandingPageBody';
+import { ProjectsPageBody } from './components/ProjectsPageBody';
+import { AboutPageBody } from './components/AboutPageBody';
+import { ContactPageBody } from './components/ContactPageBody';
 
 function App() {
 
@@ -10,6 +13,9 @@ function App() {
       <div className='flex flex-col w-[900px]'>
         <NavPannel />
         <LandingPageBody />
+        <ProjectsPageBody />
+        <AboutPageBody />
+        <ContactPageBody />
       </div>
       
     </>

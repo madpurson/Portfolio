1cb2bd043074322f1a6ea008b380a117e8a173e7
Miki <110220534+madpurson@users.noreply.github.com>
2025-10-01 23:36:54 +0800

    Update ProjectsPageBody.jsx

--- portfolio/src/components/ProjectsPageBody.jsx
+++ portfolio/src/components/ProjectsPageBody.jsx
@@ -6,29 +6,29 @@ export function ProjectsPageBody() {
             <h1 className="object-top-left text-start">
                 Projects.
             </h1>
-            <div>
-                <div>
-                    <img src="vector2.png" alt="projects" />
+            <div className="grid grid-cols-3">
+                <div className="w-full">
+                    <img className="w-full" src="vector2.png" alt="projects" />
                     <span>MeowgAw.</span>
                 </div>
-                <div>
-                    <img src="vector2.png" alt="projects" />
+                <div className="w-full">
+                    <img className="w-full" src="vector2.png" alt="projects" />
                     <span>Chiron.</span>
                 </div>
-                <div>
-                    <img src="vector2.png" alt="projects" />
+                <div className="w-full">
+                    <img className="w-full" src="vector2.png" alt="projects" />
                     <span>UPV Pamplona.</span>
                 </div>
-                <div>
-                    <img src="vector2.png" alt="projects" />
+                <div className="w-full">
+                    <img className="w-full" src="vector2.png" alt="projects" />
                     <span>eHalalan.</span>
                 </div>
-                <div>
-                    <img src="vector2.png" alt="projects" />
+                <div className="w-full">
+                    <img className="w-full" src="vector2.png" alt="projects" />
                     <span>Ganaps.</span>
                 </div>
-                <div>
-                    <img src="vector2.png" alt="projects" />
+                <div className="w-full">
+                    <img className="w-full" src="vector2.png" alt="projects" />
                     <span>Katipunan.</span>
                 </div>
                 

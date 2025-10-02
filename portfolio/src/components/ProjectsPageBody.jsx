971d501be14abf2d55ec9deac3e2ec5d5092b9a4
Miki <110220534+madpurson@users.noreply.github.com>
2025-10-02 23:34:44 +0800

    resize project width

--- portfolio/src/components/ProjectsPageBody.jsx
+++ portfolio/src/components/ProjectsPageBody.jsx
@@ -2,54 +2,59 @@ import React from "react";
 
 export function ProjectsPageBody() {
     return (
-        <>
-            <h1 className="object-top-left text-start">
-                Projects.
-            </h1>
-            <div className="grid grid-cols-3">
-                <a href="https://meowgaw.vercel.app/">
-                    <div className="w-full">
-                        <img className="w-full" src="vector2.png" alt="projects" />
-                        <span>MeowgAw.</span>
-                    </div>
-                </a>
+        <> 
+            <div className="h-screen">
+                <h1 className="object-top-left text-start">
+                    Projects.
+                </h1>
+                <div className="grid grid-cols-3">
+                    <a href="https://meowgaw.vercel.app/">
+                        <div className="w-9/10">
+                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <span>MeowgAw.</span>
+                        </div>
+                    </a>
 
-                <a href="https://chiron-s5jp5b1ed-iammichaelps-projects.vercel.app/">
-                    <div className="w-full">
-                        <img className="w-full" src="vector2.png" alt="projects" />
-                        <span>Chiron.</span>
-                    </div>
-                </a>
+                    <a href="https://chiron-s5jp5b1ed-iammichaelps-projects.vercel.app/">
+                        <div className="w-9/10 flex-1">
+                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <span>Chiron.</span>
+                        </div>
+                    </a>
 
-                <a href="https://github.com/madpurson/UPV-Pamplona">
-                    <div className="w-full">
-                        <img className="w-full" src="vector2.png" alt="projects" />
-                        <span>UPV Pamplona.</span>
-                    </div>
-                </a>
+                    <a href="https://github.com/madpurson/UPV-Pamplona">
+                        <div className="w-9/10">
+                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <span>UPV Pamplona.</span>
+                        </div>
+                    </a>
 
-                <a href="https://github.com/eHalalan/ehalalan">
-                    <div className="w-full">
-                        <img className="w-full" src="vector2.png" alt="projects" />
-                        <span>eHalalan.</span>
-                    </div>
-                </a>
+                    <a href="https://github.com/eHalalan/ehalalan">
+                        <div className="w-9/10">
+                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <span>eHalalan.</span>
+                        </div>
+                    </a>
+                    
+                    <a href="https://github.com/madpurson/DiceDiceDice">
+                        <div className="w-9/10">
+                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <span>DiceDiceDice.</span>
+                        </div>
+                    </a>
+                    
                 
-                <a href="https://github.com/madpurson/DiceDiceDice">
-                     <div className="w-full">
-                        <img className="w-full" src="vector2.png" alt="projects" />
-                        <span>DiceDiceDice.</span>
-                    </div>
-                </a>
-                
-               
-
-                <div className="w-full">
-                    <img className="w-full" src="vector2.png" alt="projects" />
-                    <span>Katipunan.</span>
+                    <a href="">
+                        <div className="w-9/10">
+                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <span>Katipunan.</span>
+                        </div>
+                    </a>
+                    
+                    
                 </div>
-                
             </div>
+            
         </>
     )
 }

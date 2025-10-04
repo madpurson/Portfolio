9e62b11f0f0119e510da127d1697858f545f9ecb
Miki <110220534+madpurson@users.noreply.github.com>
2025-10-04 23:26:59 +0800

    Update ProjectsPageBody.jsx

--- portfolio/src/components/ProjectsPageBody.jsx
+++ portfolio/src/components/ProjectsPageBody.jsx
@@ -10,35 +10,35 @@ export function ProjectsPageBody() {
                 <div className="grid grid-cols-3">
                     <a href="https://meowgaw.vercel.app/">
                         <div className="w-9/10">
-                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <img className="w-9/10 object-contain" src="/images/vector2.png" alt="projects" />
                             <span>MeowgAw.</span>
                         </div>
                     </a>
 
                     <a href="https://chiron-s5jp5b1ed-iammichaelps-projects.vercel.app/">
                         <div className="w-9/10 flex-1">
-                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <img className="w-9/10 object-contain" src="/images/vector2.png" alt="projects" />
                             <span>Chiron.</span>
                         </div>
                     </a>
 
                     <a href="https://github.com/madpurson/UPV-Pamplona">
                         <div className="w-9/10">
-                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <img className="w-9/10 object-contain" src="/images/vector2.png" alt="projects" />
                             <span>UPV Pamplona.</span>
                         </div>
                     </a>
 
                     <a href="https://github.com/eHalalan/ehalalan">
                         <div className="w-9/10">
-                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <img className="w-9/10 object-contain" src="/images/vector2.png" alt="projects" />
                             <span>eHalalan.</span>
                         </div>
                     </a>
                     
                     <a href="https://github.com/madpurson/DiceDiceDice">
                         <div className="w-9/10">
-                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <img className="w-9/10 object-contain" src="/images/vector2.png" alt="projects" />
                             <span>DiceDiceDice.</span>
                         </div>
                     </a>
@@ -46,7 +46,7 @@ export function ProjectsPageBody() {
                 
                     <a href="">
                         <div className="w-9/10">
-                            <img className="w-9/10 object-contain" src="vector2.png" alt="projects" />
+                            <img className="w-9/10 object-contain" src="/images/vector2.png" alt="projects" />
                             <span>Katipunan.</span>
                         </div>
                     </a>

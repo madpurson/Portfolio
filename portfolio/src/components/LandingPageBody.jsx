f3adfe59a304c375fa791293794d9cac665537ba
Miki <pelegrinompatrick@gmail.com>
2025-12-26 22:48:12 +0800

    added color main

--- portfolio/src/components/LandingPageBody.jsx
+++ portfolio/src/components/LandingPageBody.jsx
@@ -4,7 +4,7 @@ import tailwindConfig from "../../tailwind.config";
 export function LandingPageBody() {
     return (
         <div className="mt-10 h-screen ">
-            <h1 className="object-top-left text-start text-dark-500 text-4xl font-bold">
+            <h1 className="object-top-left text-start text-dark-500 text-4xl font-bold color-main">
                 Michael Patrick Pelegrino.
             </h1>
             <h3 className="text-start text-midnight">

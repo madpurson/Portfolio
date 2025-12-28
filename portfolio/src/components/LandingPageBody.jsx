121e36c243ccc390dbb9a56da39e798c9291b584
Miki <pelegrinompatrick@gmail.com>
2025-12-28 18:16:17 +0800

    adjusted the bottom frame of the landing page

--- portfolio/src/components/LandingPageBody.jsx
+++ portfolio/src/components/LandingPageBody.jsx
@@ -3,14 +3,14 @@ import tailwindConfig from "../../tailwind.config";
 
 export function LandingPageBody() {
     return (
-        <div className="mt-10 h-screen ">
+        <div className="mt-10 h-screen landing-page-body relative">
             <h1 className="object-top-left text-start text-dark-500 text-4xl font-bold color-main font-delta">
                 Michael Patrick Pelegrino.
             </h1>
             <h3 className="text-start text-rose-50">
                 Software Developer
             </h3>
-            <div className="flex flex-row items-end">
+            <div className="flex flex-row items-end inset-x-0 bottom-50 absolute">
                 <div className="text-start w-3/5 pr-10 text-rose-50">
                     <p>
                         I’m a software developer with a focus on backend systems, full-stack web development, and building real-world solutions. I enjoy working on meaningful projects that combine code, creativity, and impact.
@@ -20,7 +20,8 @@ export function LandingPageBody() {
                         hover:bg-rose-800 hover:scale-105 transition-all duration-200 ease-in-out">
                             Hire me
                         </button>
-                        <button className="flex grow rounded-md text-rose-50 p-3 justify-center bg-amber-950">
+                        <button className="flex grow rounded-md text-rose-50 p-3 justify-center bg-amber-950 cursor-pointer
+                        hover:bg-amber-800 hover:scale-105 transition-all duration-200 ease-in-out">
                             Download resume
                         </button>
                     </div>

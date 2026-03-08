13cb4148f86bd1d4aea92dd9d8c4deac421b2a90
Miki <110220534+madpurson@users.noreply.github.com>
2026-03-08 23:38:54 +0800

    added speed insights from vercel

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,12 +5,14 @@ import { LandingPageBody } from './screens/LandingPageBody';
 import { ProjectsPageBody } from './screens/ProjectsPageBody';
 import { AboutPageBody } from './screens/AboutPageBody';
 import { ContactPageBody } from './screens/ContactPageBody';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 function App() {
 
   return (
     <>
       <div className='flex flex-col w-[900px] object-center'>
+        <SpeedInsights />
         {/* <NavPannel /> */}
         <LandingPageBody />
         <ProjectsPageBody />

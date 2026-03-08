cf245775c28dbf6576f293530f339292063bb376
Miki <110220534+madpurson@users.noreply.github.com>
2026-03-08 23:01:44 +0800

    Update App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import { NavPannel } from './components/NavPannel'
-import { LandingPageBody } from './components/LandingPageBody';
-import { ProjectsPageBody } from './components/ProjectsPageBody';
-import { AboutPageBody } from './components/AboutPageBody';
-import { ContactPageBody } from './components/ContactPageBody';
+import { LandingPageBody } from './screens/LandingPageBody';
+import { ProjectsPageBody } from './screens/ProjectsPageBody';
+import { AboutPageBody } from './screens/AboutPageBody';
+import { ContactPageBody } from './screens/ContactPageBody';
 
 function App() {
 

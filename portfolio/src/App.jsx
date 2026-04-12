76b793b21628b27bf5cf7e73e04122885851ae0d
Miki <110220534+madpurson@users.noreply.github.com>
2026-04-12 18:35:33 +0800

    added the projects:id page in the route

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPage from './screens/MainPage';
-// import ProjectDetails from './screens/ProjectDetails';
+import ProjectDetails from './screens/ProjectDetails';
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        {/* <Route path="/projects/:id" element={<ProjectDetails />} /> */}
+        <Route path="/projects/:id" element={<ProjectDetails />} />
       </Routes>
     </BrowserRouter>
   )

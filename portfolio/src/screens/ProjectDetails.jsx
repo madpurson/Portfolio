1ca128950d72150c3c01fb435ce2f11d8a0bb412
Miki <110220534+madpurson@users.noreply.github.com>
2026-04-12 18:35:50 +0800

    added folder and the base components for the projects:id page

--- portfolio/src/screens/ProjectDetails.jsx
+++ portfolio/src/screens/ProjectDetails.jsx
@@ -1,12 +1,43 @@
-const ProjectDetails = (projectDetails) => {
-    if (!projectDetails) throw new Error('Project details not found');
-    const { title, description, image } = projectDetails;
-    if (!title || !description || !image) throw new Error('Project details are missing required fields');
+import { useParams } from "react-router-dom";
+import { projects } from "../assets/data/projects";
+
+export const ProjectDetails = () => {
+    const { id } = useParams();
+    console.log('Project ID from URL parameters:', id);
+    if (!id || isNaN(id) || typeof(id) !== 'string') console.error('Project ID is missing or invalid from URL parameters');
+    console.log('Projects data:', projects);
+    const projectDetails = projects.find((project) => project.id === id);
+    console.log('Project details found:', projectDetails);
+    // if (!projectDetails) throw new Error('Project details not found');
+    const { title, description, image, url } = projectDetails;
+    // if (!title || !description || !image) throw new Error('Project details are missing required fields');
 
 
-    return <>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <img src={image} alt={title} />
-    </>
+    return (
+        <>
+            <div className='flex flex-col w-[900px] object-center'>
+
+                <div className="page-body">
+                    <div className="page-header header-container">
+                        <h1 className="primary-h1 primary-color">
+                            {`${title}.`}
+                        </h1>
+                    </div>
+                    <div className="opened-folder">
+                        <div className="opened-folder-content">
+                            <span className="left-side">
+                                <div className="opened-folder-left-content"></div>
+                            </span>
+                            <span className="right-side">
+                                <div className="opened-folder-right-content"></div>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    )
 }
+
+export default ProjectDetails;

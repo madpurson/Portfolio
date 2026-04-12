95a31bd41ce03325eeba1469cfc83e25ad96ebf5
Miki <110220534+madpurson@users.noreply.github.com>
2026-04-12 16:21:45 +0800

    Added router

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,26 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import { NavPannel } from './components/NavPannel'
-import { LandingPageBody } from './screens/LandingPageBody';
-import { ProjectsPageBody } from './screens/ProjectsPageBody';
-import { AboutPageBody } from './screens/AboutPageBody';
-import { ContactPageBody } from './screens/ContactPageBody';
-import { SpeedInsights } from "@vercel/speed-insights/react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainPage from './screens/MainPage';
+// import ProjectDetails from './screens/ProjectDetails';
 
 function App() {
 
   return (
-    <>
-      <div className='flex flex-col w-[900px] object-center'>
-        <SpeedInsights />
-        {/* <NavPannel /> */}
-        <LandingPageBody />
-        <ProjectsPageBody />
-        {/* <AboutPageBody />
-        <ContactPageBody /> */}
-      </div>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        {/* <Route path="/projects/:id" element={<ProjectDetails />} /> */}
+      </Routes>
+    </BrowserRouter>
   )
 }
 

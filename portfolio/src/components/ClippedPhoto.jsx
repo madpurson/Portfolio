0dd35a783b09e0d6c85511ab2f58c94f1c7f4fc9
Miki <110220534+madpurson@users.noreply.github.com>
2026-04-17 00:01:14 +0800

    added conditional rendering for shuffle and sidle

--- portfolio/src/components/ClippedPhoto.jsx
+++ portfolio/src/components/ClippedPhoto.jsx
@@ -1,10 +1,22 @@
 import PaperClip from "../assets/svg/paper-clip.svg?react";
+import { useState } from "react";
 
-const ClippedPhoto = ({ imagePath, projectName }) => {
+const ClippedPhoto = ({ imagePath, projectName, handleShuffle = () => {}, shuffled = false }) => {
+    const [shuffleDeg, setShuffleDeg] = useState(0);
+    const handleReceiptShuffle = () => {
     
+    const deg = (Math.random() * 10 - 5).toFixed(2);
+    setShuffleDeg(deg);
+    handleShuffle()
+    }
     return (
-        <div className="clipped-photo-container">
-            <PaperClip className="paper-clip" />
+        <div 
+        style={{ transform: `rotate(${shuffleDeg}deg)`, zIndex: `${shuffled ? 3 : 4}` }}
+        className="clipped-photo-container" 
+        onClick={(e)=>{
+            // e.stopPropagation();
+            handleReceiptShuffle();
+        }}>
             <div className="project-photo-container">
                 <img className="project-photo" src={imagePath} alt={`${projectName} project image`} />
             </div>

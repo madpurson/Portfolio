ed1ca0274bdd4229575f17d1bf0d5f533c751124
Miki <110220534+madpurson@users.noreply.github.com>
2026-04-16 14:29:57 +0800

    Added a flipping receipt stack when clicked

--- portfolio/src/screens/ProjectDetails.jsx
+++ portfolio/src/screens/ProjectDetails.jsx
@@ -1,5 +1,10 @@
+
 import { useParams } from "react-router-dom";
 import { projects } from "../assets/data/projects";
+import TechStackReceipt from "../components/TechStackReceipt";
+import OverviewPaperDocument from "../components/OverviewPaperDocument";
+import ClippedPhoto from "../components/ClippedPhoto";
+import React from "react";
 
 export const ProjectDetails = () => {
     const { id } = useParams();
@@ -7,10 +12,14 @@ export const ProjectDetails = () => {
     if (!id || isNaN(id) || typeof(id) !== 'string') console.error('Project ID is missing or invalid from URL parameters');
     console.log('Projects data:', projects);
     const projectDetails = projects.find((project) => project.id === id);
+    const projectIndex = projects.findIndex((project) => project.id === id);
     console.log('Project details found:', projectDetails);
     // if (!projectDetails) throw new Error('Project details not found');
-    const { title, description, image, url } = projectDetails;
+    const { title, description, image, url, tagline, problem, solution } = projectDetails;
+    const PERSONAL_GITHUB_URL = "https://github.com/madpurson";
     // if (!title || !description || !image) throw new Error('Project details are missing required fields');
+    const [isReceiptShuffled, setIsReceiptShuffled] = React.useState(false);
+    const [isReceiptOpen, setIsReceiptOpen] = React.useState(false);
 
 
     return (
@@ -30,10 +39,57 @@ export const ProjectDetails = () => {
                             <span className="left-side">
                                 <img className="binder-clip" src="/images/binder-clip.png" alt="binder-clip" />
 
-                                <div className="left-content"></div>
+                                <div className="left-content">
+                                    
+                                </div>
+                                
+                                   
+                                <div onClick={() => setIsReceiptOpen(true)} className={"techstack-receipt right-techstack " + (isReceiptOpen ? "opened" : "")}>
+                                   <ClippedPhoto 
+                                //    shuffled={"shuffled"}
+                                //    handleShuffle={() => setIsReceiptShuffled(!isReceiptShuffled)}
+                                    imagePath={projectDetails.assets} 
+                                    projectName={title}
+                                    />
+                                    <TechStackReceipt 
+                                    // shuffled={"shuffled"}
+                                    // handleShuffle={() => setIsReceiptShuffled(!isReceiptShuffled)}
+                                    title={title} 
+                                    index={projectIndex}
+                                    items={projectDetails.techStack.map((tech) => ({ name: tech }))} 
+                                    footerNote="WE LOVE TO HEAR FROM YOU!" 
+                                    url={url || PERSONAL_GITHUB_URL}
+                                    />
+                                </div>
+                                
                             </span>
                             <span className="right-side">
-                                <img src="/images/paper.png" alt={title} className="right-content paper-content"></img>
+                                {/* <img src="/images/paper.png" alt={title} className="right-content paper-content"></img> */}
+                                <OverviewPaperDocument 
+                                referenceId={`ORDER #0${projectIndex + 1}`}
+                                title={title}
+                                label={tagline}
+                                date={new Date().toLocaleDateString()}
+                                problemStatement={problem || "Problem statement not provided."}
+                                solutionStatement={solution || "Solution statement not provided."}
+                                projectSummary={description || "Project summary not provided."}
+                               
+                                />
+                                <div onClick={() => setIsReceiptOpen(true)} className={"techstack-receipt right-techstack " + (isReceiptOpen ? "" : "opened")}>
+                                   <ClippedPhoto 
+                                   
+                                    imagePath={projectDetails.assets} 
+                                    projectName={title}
+                                    />
+                                    <TechStackReceipt 
+                                   
+                                    title={title} 
+                                    index={projectIndex}
+                                    items={projectDetails.techStack.map((tech) => ({ name: tech }))} 
+                                    footerNote="WE LOVE TO HEAR FROM YOU!" 
+                                    url={url || PERSONAL_GITHUB_URL}
+                                    />
+                                </div>
                             </span>
                         </div>
                     </div>
